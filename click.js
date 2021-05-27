f427c73d60928dbbab8e01ad852861a61c7ff5a6
GetGud1 <63851456+GetGud1@users.noreply.github.com>
2021-05-27 16:17:07 +0800

    Add files via upload

---
+++ click.js
@@ -0,0 +1,26 @@
+var container =  document.getElementById("remote2");
+function change1(image){
+    container.src = image.src;
+}
+
+var container1 =  document.getElementById("remote3");
+function change2(image){
+    container1.src = image.src;
+}
+
+var container2 =  document.getElementById("remote4");
+function change3(image){
+    container2.src = image.src;
+}
+
+var contain =  document.getElementById("remote1");
+function change(image){
+    contain.src = image.src;
+}
+
+// LANDING PAGE ANIMATION
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+
+
+
---
+++ coming.js
@@ -0,0 +1,33 @@
+const countdown = document.querySelector("#countdown");
+// Set Launch Date (ms)
+const launchDate = new Date("Apr 30, 2022 13:00:00").getTime();
+// Update every second
+const intvl = setInterval(() => {
+	// Get todays date and time (ms)
+	const now = new Date().getTime();
+	// Distance from now and the launch date (ms)
+	const distance = launchDate - now;
+	// Time calculation
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	const hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	);
+	const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	// Display result
+	countdown.innerHTML = `
+    <div>${days}D
+    ${hours}H
+    ${mins}M
+    ${seconds}S</div>
+    `;
+	// If launch date is reached
+	if (distance < 0) {
+		// Stop countdown
+		clearInterval(intvl);
+		// Style and output text
+		countdown.style.color = "#17a2b8";
+		// Text Display when time is finished
+		countdown.innerHTML = "Time Up !";
+	}
+}, 1000);
